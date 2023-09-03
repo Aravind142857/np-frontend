af9aad48d61341432dfb29d10e38125e98d2d083
Aravind142857 <aravind.s2025@berkeley.edu>
2023-09-03 02:43:21 -0700

    Added buttons for add and delete, search bar and number of notes

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,15 @@ import { Inter } from 'next/font/google'
 import { useEffect } from 'react'
 import dateFormat from 'dateformat'
 import { useRouter } from 'next/router'
+import {FcSearch} from "react-icons/fc"
+import {IoAddOutline} from "react-icons/io5"
+import {MdDelete} from "react-icons/md"
+import { FaDivide } from 'react-icons/fa'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({data,error}) {
+
   const router = useRouter()
   useEffect(()=>{
     console.log('process.env.NEXT_PUBLIC_BASE_URL', process.env.NEXT_PUBLIC_BASE_URL);
@@ -24,6 +29,23 @@ export default function Home({data,error}) {
       </Head>
       <main className='p-6 px-[30%]'>
       <h1 className='text-4xl text-center underline font-serif mb-12'>My notes</h1>  
+      <div className='flex justify-between w-full bg-gray-600 my-8 self-center rounded-lg p-4'>
+        <div className='text-white text-lg left-0 font-bold font-serif self-center'> {Object.keys(data).length} notes</div>
+        <div className='self-center'>
+          <div className='rounded-full bg-white hover:bg-green-300 flex items-center justify-center p-2' onClick={()=>handleNewNote()}>
+            <IoAddOutline size={24} color="black" className='self-center'/>
+          </div>
+        </div>
+        <div className='self-center'>
+          <div className='rounded-full bg-white hover:bg-red-300 flex items-center justify-center p-2' onClick={()=>handleNewNote()}>
+            <MdDelete size={24}  color="black" className='self-center'/>
+          </div>
+        </div>
+        <span className='flex-row flex bg-white'>
+          <FcSearch size={17} color='green' className='self-center mx-2 peer'/>
+          <input type='search' className='text-fuchsia-900 px-4 py-2 rounded-md focus:outline-none focus:outline-4 focus:outline-offset-0 focus:outline-fuchsia-500/80' placeholder='Search for a note'/>
+        </span>
+      </div>
       {error&&<p>{JSON.stringify(error)}</p>}
       {data.map((element)=> <div key={element.slug} className='flex justify-center items-center mb-10'>
         <div className='bg-yellow-700 my-2 p-4 shadow-lg shadow-yellow-800 rounded-lg cursor-pointer w-[100%]' onClick={()=>handleNavigation(element)}>
@@ -45,6 +67,7 @@ export async function getStaticProps(){
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/notes`);
     data = await response.json();
+    
 }
 catch(err) {
   error = err.message?err.message:"Something went wrong"
