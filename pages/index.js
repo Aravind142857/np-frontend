16a7f7a3e4d4eee297297c84f96e64c191983691
Aravind142857 <aravind.s2025@berkeley.edu>
2023-09-04 02:16:24 -0700

    Changes to aesthetics

--- pages/index.js
+++ pages/index.js
@@ -27,21 +27,21 @@ export default function Home({data,error}) {
         <title>Note Manager: | Home</title>
         <meta name='description' content='A site for notes' />
       </Head>
-      <main className='p-6 px-[30%]'>
+      <main className='p-6 xl:px-[30%] lg:px-[25%] px-[10%]'>
       <h1 className='text-4xl text-center underline font-serif mb-12'>My notes</h1>  
-      <div className='flex justify-between w-full bg-gray-600 my-8 self-center rounded-lg p-4'>
-        <div className='text-white text-lg left-0 font-bold font-serif self-center'> {Object.keys(data).length} notes</div>
-        <div className='self-center'>
+      <div className='flex xl:flex-row flex-col flex-shrink justify-between items-center w-full bg-gray-600 my-8 self-center rounded-lg p-4'>
+        <div className='xl:my-0 my-1 rounded-lg p-1 text-white text-lg left-0 bg-pink-600 selection:bg-black selection:text-white w-fit font-bold font-serif self-center text-center'> {Object.keys(data).length} notes</div>
+        <div className='xl:my-0 my-1 self-center w-fit'>
           <div className='rounded-full bg-white hover:bg-green-300 flex items-center justify-center p-2'>
             <IoAddOutline size={24} color="black" className='self-center'/>
           </div>
         </div>
-        <div className='self-center'>
+        <div className='xl:my-0 my-1 self-center w-fit'>
           <div className='rounded-full bg-white hover:bg-red-300 flex items-center justify-center p-2'>
             <MdDelete size={24}  color="black" className='self-center'/>
           </div>
         </div>
-        <span className='flex-row flex bg-white'>
+        <span className='xl:my-0 my-1 flex-row flex bg-white'>
           <FcSearch size={17} color='green' className='self-center mx-2 peer'/>
           <input type='search' className='text-fuchsia-900 px-4 py-2 rounded-md focus:outline-none focus:outline-4 focus:outline-offset-0 focus:outline-fuchsia-500/80' placeholder='Search for a note'/>
         </span>
